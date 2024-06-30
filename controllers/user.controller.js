60e9b390a36ed8c6f52d2300bf4471d59f1bfb0a
matiasolocco <matias.olocco@hotmail.com>
2024-06-30 13:58:37 +0200

    admin/user printing WORK S

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -70,7 +70,7 @@ const userController = {
     getAllUsers: async (req, res) => {
         try {
             const users = await User.find();
-            res.status(200).json({ users });
+            res.status(200).json(users);
         } catch (error) {
             res.status(500).json({ message: 'Error al obtener los usuarios', error: error.message });
         }
