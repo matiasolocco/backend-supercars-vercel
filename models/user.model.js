8a3521530604873b3686f5a3b4639eea88c1a572
matiasolocco <matias.olocco@hotmail.com>
2024-07-21 22:10:21 +0200

    Provisional backend

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    name: {
+    userName: {
         type: String,
         required: true
     },
